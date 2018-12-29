963985f0fd4b39cc88cc30eda2b1e2b07cd4508d
sidiquimk <masoodsdq1@gmail.com>
2018-12-29 11:12:29 -0500

    initial commit

---
+++ src/test/java/com/MavenTestNG/BaseClass.java
@@ -0,0 +1,17 @@
+package com.MavenTestNG;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.annotations.Test;
+
+public class BaseClass {
+	
+	@Test
+	public void setup (){
+		System.setProperty("webdriver.chrome.driver", "drivers/chromedriver.exe");
+		WebDriver driver = new ChromeDriver();
+		
+		driver.get("https://www.google.com.ar/");
+	}
+
+}
